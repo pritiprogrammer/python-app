7ea987cf13e349138b6111fd8506f581f18d3e7f
Pritigamot94 <preetigamot@gmail.com>
2026-09-12 17:34:42 -0500

    adds ci git hub workflow

--- src/app.py
+++ src/app.py
@@ -1,5 +1,5 @@
-# '/api/V1/details'
-# '/api/V1/healthz'
+# '/api/v1/details'
+# '/api/v1/healthz'
 from flask import Flask,jsonify
 import datetime;
 import socket
@@ -7,13 +7,13 @@ import socket
 app = Flask(__name__)
 hostname = socket.gethostname()
 
-@app.route('/api/V1/details')
+@app.route('/api/v1/details')
 def hello_world():
     return jsonify({'time': datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
                     'hostname': hostname,
                      'message': 'Hello World', 'status': 'success', 'code': 200,'title':'Flask API'}),200
 
-@app.route('/api/V1/healthz')
+@app.route('/api/v1/healthz')
 def health_check():
     return jsonify({'time': datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"), 'hostname': hostname, 'message': 'Health Check', 'status': 'success', 'code': 200,'title':'Flask API'}),200
 if __name__ == '__main__':
